a199379590ee8c0139adde88c2e2163be6773024
Schae Stewart <schaestewart@gmail.com>
2019-04-23 14:04:26 -0400

    [wip] More progress with reading chunks

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,7 +6,6 @@ export function format(first: string, middle: string, last: string): string {
 
 export async function unbakeBadge(badgeUrl: string): Promise<string> {
   const PNG_SIGNATURE = [137, 80, 78, 71, 13, 10, 26, 10];
-
   const ensurePNGsignature = (
     buffer: ArrayBuffer,
     PNG_SIGNATURE: Array<Number>
@@ -21,148 +20,28 @@ export async function unbakeBadge(badgeUrl: string): Promise<string> {
     }
   };
 
-  const reverse = (x: any, n: any): any => {
-    let b = 0;
-    while (--n >= 0) {
-      b <<= 1;
-      b |= x & 1;
-      x >>>= 1;
-    }
-    return b;
-    // let b = 0;
-    // while (n) {
-    //   b = b * 2 + (x % 2);
-    //   x /= 2;
-    //   x -= x % 1;
-    //   n--;
-    // }
-    // return b;
-  };
-
-  const crc32 = (s: any): any => {
-    s = String(s);
-    let polynomial = arguments.length < 2 ? 0x04c11db7 : arguments[1] >>> 0;
-    let initialValue = arguments.length < 3 ? 0xffffffff : arguments[2] >>> 0;
-    let finalXORValue = arguments.length < 4 ? 0xffffffff : arguments[3] >>> 0;
-    let table = new Array(256);
-
-    let i = -1;
-    while (++i < 256) {
-      let g = reverse(i, 32);
-      let j = -1;
-      while (++j < 8) {
-        g = ((g << 1) ^ (((g >>> 31) & 1) * polynomial)) >>> 0;
-      }
-      table[i] = reverse(g, 32);
+  const getChunks = (byteoffset, buffer: ArrayBuffer) => {
+    if (byteoffset >= buffer.byteLength) {
+      return;
     }
 
-    let crc = initialValue;
-    let length = s.length;
-    let k = -1;
-    while (++k < length) {
-      let c = s.charCodeAt(k);
-      if (c > 255) {
-        throw new RangeError();
-      }
-      let index = (crc & 255) ^ c;
-      crc = ((crc >>> 8) ^ table[index]) >>> 0;
-    }
-    return (crc ^ finalXORValue) >>> 0;
-  };
-
-  const arrayToStr = (arr: Uint8Array): String =>
-    [].map.call(arr, charCode => String.fromCharCode(charCode)).join("");
-
-  const readTextChunk = (bytes: Uint8Array, textChunks: object) => {
-    let keyword, text;
-    let i = 0;
-    while (i < bytes.length) {
-      if (bytes[i] == 0) {
-        keyword = arrayToStr([].slice.call(bytes, 0, i));
-        text = arrayToStr([].slice.call(bytes, i + 1));
-        break;
-      }
-      i++;
-    }
-    if (!keyword) {
-      throw new Error("malformed tEXt chunk");
-    }
-
-    textChunks[keyword] = text;
-  };
-
-  const readiTxtChunk = (bytes: Uint8Array, textChunks: object) => {
-    let nullsEncountered = 0;
-    let i = 0;
-    let text: String;
-    let keyword;
-
-    while (i < bytes.length) {
-      if (bytes[i] == 0 && nullsEncountered == 0) {
-        keyword = arrayToStr([].slice.call(bytes, 0, i));
-        nullsEncountered++;
-      } else if (bytes[i] == 0 && nullsEncountered == 3) {
-        text = arrayToStr([].slice.call(bytes, i + 1));
-        break;
-      } else if (bytes[i] == 0) {
-        nullsEncountered++;
-      }
-      i++;
-    }
-
-    if (!keyword) {
-      throw new Error("malformed iTxt chunk");
-    }
-
-    textChunks[keyword] = text;
-  };
-
-  // https://github.com/curlee/png-baker.js/blob/gh-pages/src/PNGBaker.litcoffee
-
-  const readNextChunk = (
-    buffer: ArrayBuffer,
-    byteOffset: number,
-    textChunks: object,
-    chunks: Array<object>
-  ) => {
-    let i = byteOffset;
     const data = new DataView(buffer);
 
-    const chunkLength = data.getUint32(i);
-    i += 4;
+    const chunkLength = data.getUint32(byteoffset);
+    const chunkType = new Uint8Array(buffer, byteoffset + 4, 4).reduce(
+      (acc, val) => `${acc}${String.fromCharCode(val)}`,
+      ""
+    );
+    const chunkData = new Uint8Array(buffer, byteoffset, chunkLength);
 
-    const crcData = new Uint8Array(buffer, i, chunkLength + 4);
-    const ourCRC = crc32(crcData);
+    // TODO: probably check the CRC?
 
-    const chunkType = arrayToStr(new Uint8Array(buffer, i, 4));
+    console.log({ chunkLength, chunkType, chunkData });
 
-    const chunkBytes = new Uint8Array(buffer, i, chunkLength);
-    i += chunkLength;
-
-    const chunkCRC = data.getUint32(i);
-    i += 4;
-
-    if (chunkCRC != ourCRC) {
-      throw new Error("CRC mismatch for chunk type " + chunkType);
-    }
-
-    if (chunkType == "tEXt") {
-      readTextChunk(chunkBytes, textChunks);
-    } else if (chunkType == "iTXt") {
-      readiTxtChunk(chunkBytes, textChunks);
-    } else {
-      chunks.push({
-        type: chunkType,
-        data: chunkBytes
-      });
-    }
-
-    return i - byteOffset;
+    // Each chunk has 4 bytes for length, 4 bytes for type, 4 bytes for crc, and then X bytes for data
+    const totalChunkLength = chunkLength + 12;
+    getChunks(byteoffset + totalChunkLength, buffer);
   };
-
-  // Is mutated as a side effect by readTextChunk and readItxtChunk(?)
-  const textChunks = {};
-  const chunks = []; // Also mutated
 
   const imgBuffer = await fetch(
     `https://cors-anywhere.herokuapp.com/${badgeUrl}`
@@ -170,12 +49,7 @@ export async function unbakeBadge(badgeUrl: string): Promise<string> {
 
   ensurePNGsignature(imgBuffer, PNG_SIGNATURE);
 
-  let i = PNG_SIGNATURE.length;
-  while (i < imgBuffer.byteLength) {
-    i += readNextChunk(imgBuffer, i, textChunks, chunks);
-  }
-
-  console.log({ textChunks, chunks });
+  getChunks(PNG_SIGNATURE.length, imgBuffer);
 
   return "";
 }
