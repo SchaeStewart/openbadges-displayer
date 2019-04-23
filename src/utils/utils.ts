24c4e6c0b06d86ca621858ada88166273721c529
Schae Stewart <schaestewart@gmail.com>
2019-04-23 16:31:20 -0400

    Refacotr unbake method

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,7 +4,11 @@ export function format(first: string, middle: string, last: string): string {
   );
 }
 
-export async function unbakeBadge(badgeUrl: string): Promise<string> {
+export async function unbakeBadge(badgeUrl: string): Promise<object> {
+  const arrayToStr = (acc: string, val: number): string =>
+    `${acc}${String.fromCharCode(val)}`;
+
+  //TODO: fix types
   const PNG_SIGNATURE = [137, 80, 78, 71, 13, 10, 26, 10];
   const ensurePNGsignature = (
     buffer: ArrayBuffer,
@@ -21,15 +25,15 @@ export async function unbakeBadge(badgeUrl: string): Promise<string> {
   };
 
   const getChunks = (byteoffset, buffer: ArrayBuffer): Array<any> => {
+    // TODO: error check and ensure last and first chunks are IHDR and IEND
     if (byteoffset >= buffer.byteLength) {
       return [];
     }
 
     const data = new DataView(buffer);
-
     const chunkLength = data.getUint32(byteoffset);
     const chunkType = new Uint8Array(buffer, byteoffset + 4, 4).reduce(
-      (acc, val) => `${acc}${String.fromCharCode(val)}`,
+      arrayToStr,
       ""
     );
     const chunkData = new Uint8Array(buffer, byteoffset + 8, chunkLength);
@@ -43,64 +47,59 @@ export async function unbakeBadge(badgeUrl: string): Promise<string> {
     ];
   };
 
+  const getiTXt = chunks => {
+    return chunks
+      .filter(chunk => chunk.type === "iTXt")
+      .map(chunk => {
+        // TODO: clean this up. Make more functional. Handle compression flag
+        let nullsEncountered = 0;
+        let i = 0;
+        let bytes = chunk.data;
+        let keyword: string;
+        let text: string;
+        while (i < bytes.length) {
+          if (bytes[i] == 0 && nullsEncountered == 0) {
+            keyword = bytes.slice(0, i).reduce(arrayToStr, "");
+            nullsEncountered++;
+          } else if (bytes[i] == 0 && nullsEncountered == 4) {
+            // Why is this 4? Because the compression flag is 0 for uncompressed which was probably screwwing with this logic?
+            text = bytes.slice(i + 1).reduce(arrayToStr, "");
+            break;
+          } else if (bytes[i] == 0) {
+            nullsEncountered++;
+          }
+          i++;
+        }
+        return { keyword, text };
+      });
+  };
+
+  // const tEXt = chunks.filter(chunk => chunk.type === "tEXt");
+  // const gettEXt = chunk => {
+  //   let i = 0;
+  //   const bytes = chunk.data;
+  //   while (i < bytes.length) {
+  //     if (bytes[i] == 0) {
+  //       const keyword = bytes
+  //         .slice(0, i)
+  //         .reduce(arrayToStr, "");
+  //       const text = bytes
+  //         .slice(i + 1)
+  //         .reduce(arrayToStr, "");
+  //       return { keyword, text };
+  //     }
+  //     i++;
+  //   }
+  // };
+
   const imgBuffer = await fetch(
     `https://cors-anywhere.herokuapp.com/${badgeUrl}`
   ).then(res => res.arrayBuffer());
-
   ensurePNGsignature(imgBuffer, PNG_SIGNATURE);
-
   const chunks = getChunks(PNG_SIGNATURE.length, imgBuffer);
-  // TODO: error check and ensure last and first chunks are IHDR and IEND
-
-  const iTXt = chunks.filter(chunk => chunk.type === "iTXt");
-  const getiTXt = chunk => {
-    // TODO: clean this up. Make more functional. Handle compression flag
-    let nullsEncountered = 0;
-    let i = 0;
-    let bytes = chunk.data;
-    let keyword: string;
-    let text: string;
-    while (i < bytes.length) {
-      if (bytes[i] == 0 && nullsEncountered == 0) {
-        keyword = bytes
-          .slice(0, i)
-          .reduce((acc, val) => `${acc}${String.fromCharCode(val)}`, "");
-        nullsEncountered++;
-      } else if (bytes[i] == 0 && nullsEncountered == 4) {
-        // Why is this 4? Because the compression flag is 0 for uncompressed which was probably screwwing with this logic?
-        text = bytes
-          .slice(i + 1)
-          .reduce((acc, val) => `${acc}${String.fromCharCode(val)}`, "");
-        break;
-      } else if (bytes[i] == 0) {
-        nullsEncountered++;
-      }
-      i++;
-    }
-    return { keyword, text };
-  };
-
-  const gettEXt = chunk => {
-    let i = 0;
-    const bytes = chunk.data;
-    while (i < bytes.length) {
-      if (bytes[i] == 0) {
-        const keyword = bytes
-          .slice(0, i)
-          .reduce((acc, str) => `${acc}${String.fromCharCode(str)}`, "");
-        const text = bytes
-          .slice(i + 1)
-          .reduce((acc, str) => `${acc}${String.fromCharCode(str)}`, "");
-        return { keyword, text };
-      }
-      i++;
-    }
-  };
-
-  const tEXt = chunks.filter(chunk => chunk.type === "tEXt");
-  console.log(tEXt.map(gettEXt));
-
-  console.log(iTXt.map(getiTXt));
-
-  return "";
+  return getiTXt(chunks)
+    .filter(chunk => chunk.keyword === "openbadges")
+    .map(chunk => ({
+      text: JSON.parse(chunk.text)
+    }))[0]; // TODO: in future ensure there is only ever 1 openbadge iTXt
 }
